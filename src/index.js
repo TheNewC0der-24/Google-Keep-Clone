8357cf8e2cba203ca99cd96d3f9770e5c90ed859
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-03-19 01:16:00 +0530

    Wrap the application with Context Provider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import DataProvider from './Context/DataProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DataProvider>
+      <App />
+    </DataProvider>
   </React.StrictMode>
 );
