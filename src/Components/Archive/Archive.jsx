a6dca98aa81d618b3b65e717112697158cdef3d9
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-04-01 19:55:33 +0530

    Create Archive Notes UI and handle the archive notes functionality and unarchive notes functionality.

--- src/Components/Archive/Archive.jsx
+++ src/Components/Archive/Archive.jsx
@@ -1,8 +1,64 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 
-const Archive = () => {
+import { Card, CardActions, CardContent, IconButton, Typography, Tooltip } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+import { UnarchiveOutlined, DeleteOutlineOutlined } from '@mui/icons-material';
+
+import { DataContext } from '../../Context/DataProvider';
+
+const ArchiveCard = styled(Card)`
+    box-shadow: none;
+    border: 1px solid #e0e0e0;
+    border-radius: 8px;
+`;
+
+const Archive = ({ archiveNote }) => {
+
+    const [showActions, setShowActions] = useState(false);
+
+    const { setNotes, archivedNotes, setArchivedNotes, setDeletedNotes } = useContext(DataContext);
+
+    const unarchiveNote = (archiveNote) => {
+        const updatedNotes = archivedNotes.filter(data => data.id !== archiveNote.id);
+        setArchivedNotes(updatedNotes);
+        setNotes(prevArr => [...prevArr, archiveNote]);
+    }
+
+    const deleteNote = (archiveNote) => {
+        const updatedNotes = archivedNotes.filter(data => data.id !== archiveNote.id);
+        setArchivedNotes(updatedNotes);
+        setDeletedNotes(prevArr => [...prevArr, archiveNote]);
+    }
+
     return (
-        <div>Archive</div>
+        <ArchiveCard
+            onMouseEnter={() => setShowActions(true)}
+            onMouseLeave={() => setShowActions(false)}
+        >
+            <CardContent sx={{ wordWrap: "break-word" }}>
+                <Typography>{archiveNote.title}</Typography>
+                <Typography>{archiveNote.text}</Typography>
+            </CardContent>
+            <CardActions sx={{ display: "flex", justifyContent: "end", marginLeft: "auto" }}>
+                <Tooltip title="Unarchive">
+                    <IconButton
+                        sx={{ visibility: showActions ? 'visible' : 'hidden' }}
+                        onClick={() => unarchiveNote(archiveNote)}
+                    >
+                        <UnarchiveOutlined fontSize='small' />
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="Delete">
+                    <IconButton
+                        sx={{ visibility: showActions ? 'visible' : 'hidden' }}
+                        onClick={() => deleteNote(archiveNote)}
+                    >
+                        <DeleteOutlineOutlined fontSize='small' />
+                    </IconButton>
+                </Tooltip>
+            </CardActions>
+        </ArchiveCard>
     )
 }
 
