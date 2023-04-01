b48fb605ce214217b6fb225a9e739aa9d1a6e696
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-04-01 21:45:42 +0530

    ✨ feat: Add local storage functionality to persist notes data

--- src/Context/DataProvider.js
+++ src/Context/DataProvider.js
@@ -1,12 +1,33 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const DataContext = createContext(null);
 
 const DataProvider = ({ children }) => {
 
-    const [notes, setNotes] = useState([]);
-    const [archivedNotes, setArchivedNotes] = useState([]);
-    const [deletedNotes, setDeletedNotes] = useState([]);
+    const [notes, setNotes] = useState(() => {
+        const storedNotes = localStorage.getItem("notes");
+        return storedNotes ? JSON.parse(storedNotes) : [];
+    });
+    const [archivedNotes, setArchivedNotes] = useState(() => {
+        const storedArchivedNotes = localStorage.getItem("archivedNotes");
+        return storedArchivedNotes ? JSON.parse(storedArchivedNotes) : [];
+    });
+    const [deletedNotes, setDeletedNotes] = useState(() => {
+        const storedDeletedNotes = localStorage.getItem("deletedNotes");
+        return storedDeletedNotes ? JSON.parse(storedDeletedNotes) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes));
+    }, [notes]);
+
+    useEffect(() => {
+        localStorage.setItem("archivedNotes", JSON.stringify(archivedNotes));
+    }, [archivedNotes]);
+
+    useEffect(() => {
+        localStorage.setItem("deletedNotes", JSON.stringify(deletedNotes));
+    }, [deletedNotes]);
 
     return (
         <DataContext.Provider value={{
