b153ab6a9bb354f0306f6dbf3e8ad355be12761d
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-04-01 19:56:56 +0530

    Map the archive notes and handle the condition for no archive notes and when the archive notes are available in the Archive.

---
+++ src/Components/Archive/Archives.jsx
@@ -0,0 +1,53 @@
+import React, { useContext } from 'react';
+
+import Archive from './Archive';
+
+import { DataContext } from '../../Context/DataProvider';
+
+import { Box, Typography, Grid } from '@mui/material';
+
+import { ArchiveOutlined } from '@mui/icons-material';
+
+const Archives = () => {
+
+    const { archivedNotes } = useContext(DataContext);
+
+    return (
+        <React.Fragment>
+            {
+                archivedNotes.length === 0 ? (
+                    <Box sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        marginTop: '5rem',
+                    }}>
+                        <ArchiveOutlined sx={{
+                            backgroundSize: '120px 120px',
+                            height: '120px',
+                            margin: '20px',
+                            opacity: '.1',
+                            width: '120px',
+                        }} />
+                        <Typography sx={{ fontSize: '1.375rem' }} align='center' variant="h6" color="#5f6368">
+                            Your archived notes appear here
+                        </Typography>
+                    </Box>
+                ) :
+                    (
+                        <Grid spacing={2} container>
+                            {
+                                archivedNotes.map(archiveNote => (
+                                    <Grid item xs={12} sm={6} md={4} lg={3}>
+                                        <Archive archiveNote={archiveNote} />
+                                    </Grid>
+                                ))
+                            }
+                        </Grid>
+                    )
+            }
+        </React.Fragment>
+    )
+}
+
+export default Archives;
